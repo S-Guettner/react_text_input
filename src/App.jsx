e34df9b05fac13558e9cf7b86cc30782813900e3
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-22 14:14:26 +0100

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
         <input onChange={(e) => {setFirstName(e.target.value)}} className='border-2 border-black mx-2 mt-4' placeholder='firstName' type="text" name="firstName" id="firstName" />
         <input onChange={(e) => {setLastName(e.target.value)}} className='border-2 border-black mx-2 mt-4' placeholder='lastName' type="text" name="lastName" id="lastName" />
         <input onChange={(e) => {setEmail(e.target.value)}} className='border-2 border-black mx-2 mt-4' placeholder='email' type="email" name="email" id="email" />
-        <p>First Name : {firstName}</p>
-        <p>Last Name : {lastName}</p>
-        <p>Email : {email}</p>
+        <p className='pt-10 pl-4'>First Name : {firstName}</p>
+        <p className='pl-4'>Last Name : {lastName}</p>
+        <p className='pl-4'>Email : {email}</p>
     </div>
   )
 }
