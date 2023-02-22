c8b99b78355db9c11c6c46dc1b8aab38c14b724f
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-22 14:12:55 +0100

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
 
   return (
     <div className="App">
-    
-
+        <input onChange={(e) => {setFirstName(e.target.value)}} className='border-2 border-black mx-2 mt-4' placeholder='firstName' type="text" name="firstName" id="firstName" />
+        <input onChange={(e) => {setLastName(e.target.value)}} className='border-2 border-black mx-2 mt-4' placeholder='lastName' type="text" name="lastName" id="lastName" />
+        <input onChange={(e) => {setEmail(e.target.value)}} className='border-2 border-black mx-2 mt-4' placeholder='email' type="email" name="email" id="email" />
+        <p>First Name : {firstName}</p>
+        <p>Last Name : {lastName}</p>
+        <p>Email : {email}</p>
     </div>
   )
 }
